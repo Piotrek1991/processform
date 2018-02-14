59190fed5399fa0e20842fe29182b418d05040d8
dominisz <dominisz@gmail.com>
2018-02-14 20:13:09 +0100

    Get parameters from request

--- src/main/java/pl/dominisz/FormServlet.java
+++ src/main/java/pl/dominisz/FormServlet.java
@@ -18,10 +18,20 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String acceptance = req.getParameter("acceptance");
+        String sex = req.getParameter("sex");
+        String hardware = req.getParameter("hardware");
+
         resp.setContentType("text/html");
         resp.setCharacterEncoding("utf-8");
         PrintWriter out = resp.getWriter();
-        out.println("Processing form...");
+        out.println("<h1>Your details</h1>");
+        out.println("Username: " + username + "<br>");
+        out.println("Terms and conditions: " + acceptance + "<br>");
+        out.println("Sex: " + sex + "<br>");
+        out.println("Gaming platform: " + hardware + "<br>");
     }
 
 }
