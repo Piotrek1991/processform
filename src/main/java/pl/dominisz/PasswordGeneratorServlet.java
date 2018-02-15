e2a828fcd5aee2580ed55dbc3191ace51dc53869
dominisz <dominisz@gmail.com>
2018-02-15 18:00:31 +0100

    Create random password generator

--- src/main/java/pl/dominisz/PasswordGeneratorServlet.java
+++ src/main/java/pl/dominisz/PasswordGeneratorServlet.java
@@ -10,17 +10,25 @@ import java.io.PrintWriter;
 @WebServlet("/generatepassword")
 public class PasswordGeneratorServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    private static final String ON = "on";
 
-    }
+    private RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int length = Integer.parseInt(req.getParameter("length"));
+        boolean useSymbols = ON.equals(req.getParameter("symbols"));
+        boolean useNumbers = ON.equals(req.getParameter("numbers"));
+        boolean useLowerCase = ON.equals(req.getParameter("lowercase"));
+        boolean useUpperCase = ON.equals(req.getParameter("uppercase"));
+
+        String password = randomPasswordGenerator.generate(length,
+                useSymbols, useNumbers, useUpperCase, useLowerCase);
+
         resp.setContentType("text/html");
         resp.setCharacterEncoding("utf-8");
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Your password: ma{8eLw}%x\"%xX+b</h1>");
+        out.println("<h1>Your password: " + password + "</h1>");
     }
 
 }
---
+++ src/main/java/pl/dominisz/RandomPasswordGenerator.java
@@ -0,0 +1,11 @@
+package pl.dominisz;
+
+/**
+ * http://dominisz.pl
+ * 15.02.2018
+ */
+public class RandomPasswordGenerator {
+    public String generate(int length, boolean useSymbols, boolean useNumbers, boolean useUpperCase, boolean useLowerCase) {
+        return "asasf";
+    }
+}
